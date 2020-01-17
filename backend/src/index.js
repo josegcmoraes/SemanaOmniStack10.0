af8c706c17ca737dd6dd2df382c55675efefc8f7
jose1gcm <josegcmoraes@gmail.com>
2020-01-17 05:36:46 -0300

    Atualizando o arquivo index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 const app = express();
  
@@ -11,8 +12,8 @@ mongoose.connect('mongodb://jose:omnistack@cluster0-shard-00-00-rljxi.mongodb.ne
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });*/
-
-
+//app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3333); //
+app.listen(3333); 
