6c788ff6b7e5bfaa48ca8534c1d85ad7777f02d5
jose1gcm <josegcmoraes@gmail.com>
2020-01-19 15:55:36 -0300

    Criando a aplicação mobile

---
+++ mobile/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <>
+    <StatusBar barStyle='ight-content' backgroundColor='#7D40E7'/>
+    <Routes />
+    </>
+  );
+}
+
