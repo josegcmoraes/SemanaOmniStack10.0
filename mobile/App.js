b11839c082130374dbc5a9e169d12bf0fea69524
jose1gcm <josegcmoraes@gmail.com>
2020-01-19 23:55:07 -0300

    Criando a aplicação mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, YellowBox } from 'react-native';
 
 import Routes from './src/routes';
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket'
+]);
 
 export default function App() {
   return (
     <>
-    <StatusBar barStyle='ight-content' backgroundColor='#7D40E7'/>
-    <Routes />
+      <StatusBar barStyle='light-content' backgroundColor='#7D40E7'/>
+      <Routes />
     </>
   );
 }
