eb68791c8a578f16f11cb0e3d92ade807a30ea5a
jose1gcm <josegcmoraes@gmail.com>
2020-01-19 23:48:03 -0300

    Criando o backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
+
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket')
+
 const app = express();
- 
+const server = http.Server(app);
+
+setupWebsocket(server);
+
 mongoose.connect('mongodb://jose:omnistack@cluster0-shard-00-00-rljxi.mongodb.net:27017,cluster0-shard-00-01-rljxi.mongodb.net:27017,cluster0-shard-00-02-rljxi.mongodb.net:27017/week10?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -16,4 +23,5 @@ mongoose.connect('mongodb://jose:omnistack@cluster0-shard-00-00-rljxi.mongodb.ne
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3333); 
+
+server.listen(3333); 
