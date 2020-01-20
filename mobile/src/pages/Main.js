27cd25d7c25aa77444278cdc2d1f273b0c8d4a96
jose1gcm <josegcmoraes@gmail.com>
2020-01-19 23:52:59 -0300

    Criando a aplicação mobile

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -5,6 +5,7 @@ import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons';
 
 import api from '../services/api';
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket';
 
 function Main({ navigation }) {
   const [devs, setDevs] = useState([]);
@@ -31,6 +32,20 @@ function Main({ navigation }) {
     loadInitialPosition();
   }, []);
 
+  useEffect(() => {
+    subscribeToNewDevs(dev => setDevs([...devs, dev]));
+  }, [devs]);
+
+  function setupWebsocket() {
+    disconnect();
+    const { latitude, longitude } = currentRegion;
+    connect(
+      latitude,
+      longitude,
+      techs,
+    );
+  }
+
   async function loadDevs() {
     const { latitude, longitude} = currentRegion;
     const response = await  api.get('/search',{
@@ -42,6 +57,7 @@ function Main({ navigation }) {
     });
     console.log(response.data.devs);
     setDevs(response.data.devs);
+    setupWebsocket();
   }
 
   function handleRegionChanged(region) {
