84dc5cd248a0710f2f77779b55ec8f7749d29945
jose1gcm <josegcmoraes@gmail.com>
2020-01-19 23:54:25 -0300

    Criando a aplicação mobile

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // usar o IP da sua máquina no emulador trocar por localhost
-    baseURL: 'http://192.168.0.104:3333',
+    baseURL: 'http://192.168.2.5:3333',
 });
 
 export default api;
